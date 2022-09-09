0297edec998a8184af5ad20134a1f0071019ada7
Art Chaidarun <artchaidarun@gmail.com>
2022-09-09 14:54:10 -0400

    Audit system configs

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 set -eu
 
-create_symlink() {
+# Create missing symlinks within home directory
+ensure_symlink() {
   local -r src="${1}"
   local -r dst="${2}"
   local -r dst_dir="$(dirname "${dst}")"
@@ -18,28 +19,44 @@ create_symlink() {
     ln -s "${PWD}/${src}" "${dst}"
   fi
 }
+ensure_symlink ag/.agignore ~/.agignore
+ensure_symlink alacritty ~/.config/alacritty
+ensure_symlink code/keybindings.json ~/.config/Code/User/keybindings.json
+ensure_symlink code/settings.json ~/.config/Code/User/settings.json
+ensure_symlink easystroke ~/.easystroke
+ensure_symlink feh/.fehbg ~/.fehbg
+ensure_symlink git/.git-template ~/.git-template
+ensure_symlink git/.gitconfig ~/.gitconfig
+ensure_symlink gtk-2.0/.gtkrc-2.0 ~/.gtkrc-2.0
+ensure_symlink gtk-3.0 ~/.config/gtk-3.0
+ensure_symlink i3 ~/.config/i3
+ensure_symlink i3blocks ~/.config/i3blocks
+ensure_symlink iftop/.iftoprc ~/.iftoprc
+ensure_symlink nano/.nanorc ~/.nanorc
+ensure_symlink pylint/.pylintrc ~/.pylintrc
+ensure_symlink ripgrep/.rgignore ~/.rgignore
+ensure_symlink sublime ~/.config/sublime-text-3/Packages/User
+ensure_symlink tmux/.tmux.conf ~/.tmux.conf
+ensure_symlink virtualenvwrapper/postactivate ~/.virtualenvs/postactivate
+ensure_symlink virtualenvwrapper/postmkvirtualenv ~/.virtualenvs/postmkvirtualenv
+ensure_symlink x/.xbindkeysrc ~/.xbindkeysrc
+ensure_symlink x/.xinitrc ~/.xinitrc
+ensure_symlink x/.Xmodmap ~/.Xmodmap
+ensure_symlink zsh/.zshrc ~/.zshrc
 
-create_symlink ag/.agignore ~/.agignore
-create_symlink alacritty ~/.config/alacritty
-create_symlink code/keybindings.json ~/.config/Code/User/keybindings.json
-create_symlink code/settings.json ~/.config/Code/User/settings.json
-create_symlink easystroke ~/.easystroke
-create_symlink feh/.fehbg ~/.fehbg
-create_symlink git/.git-template ~/.git-template
-create_symlink git/.gitconfig ~/.gitconfig
-create_symlink gtk-2.0/.gtkrc-2.0 ~/.gtkrc-2.0
-create_symlink gtk-3.0 ~/.config/gtk-3.0
-create_symlink i3 ~/.config/i3
-create_symlink i3blocks ~/.config/i3blocks
-create_symlink iftop/.iftoprc ~/.iftoprc
-create_symlink nano/.nanorc ~/.nanorc
-create_symlink pylint/.pylintrc ~/.pylintrc
-create_symlink ripgrep/.rgignore ~/.rgignore
-create_symlink sublime ~/.config/sublime-text-3/Packages/User
-create_symlink tmux/.tmux.conf ~/.tmux.conf
-create_symlink virtualenvwrapper/postactivate ~/.virtualenvs/postactivate
-create_symlink virtualenvwrapper/postmkvirtualenv ~/.virtualenvs/postmkvirtualenv
-create_symlink x/.xbindkeysrc ~/.xbindkeysrc
-create_symlink x/.xinitrc ~/.xinitrc
-create_symlink x/.Xmodmap ~/.Xmodmap
-create_symlink zsh/.zshrc ~/.zshrc
+# Audit /etc and other files outside home directory
+while read -r repo_path; do
+  system_path="/${repo_path}"
+  if [[ -f ${system_path} ]]; then
+    repo_hash="$(md5sum "${repo_path}" | awk '{print $1}')"
+    system_hash="$(md5sum "${system_path}" | awk '{print $1}')"
+    if [[ ${repo_hash} == "${system_hash}" ]]; then
+      echo "Found matching ${system_path}"
+    else
+      echo "Found mismatched ${system_path}"
+      diff "${repo_path}" "${system_path}"
+    fi
+  else
+    echo "Didn't find ${system_path}"
+  fi
+done < <(find etc -type f)
