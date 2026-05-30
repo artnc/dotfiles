6253ae055d677f6f671887f9d16fda42e958e194
Art Chaidarun <art@chaidarun.com>
2026-05-29 23:18:43 -0400

    Tweak tmux bar colors

--- tmux/host-color.sh
+++ tmux/host-color.sh
@@ -6,10 +6,10 @@ colors=(
   '#5C4400' # amber
   '#6B1A1A' # crimson
   '#401A6B' # violet
-  '#1A276B' # navy
-  '#6B3A1A' # sienna
-  '#6B1A4D' # plum
   '#1A5A5A' # teal
+  '#1A276B' # navy
+  '#6B1A4D' # plum
+  '#6B3A1A' # sienna
 )
 n=${#colors[@]}
 hostname=$(cat /etc/hostname 2> /dev/null || hostname)
